1e02780515937f7f0888ba67d672c2c00b421945
MastodonCatholic <langmj02@pfw.edu>
2019-02-22 17:07:56 -0500

    operational mass time generator

--- script.js
+++ script.js
@@ -1,125 +1,78 @@
-var today = new Date();
-
-var el = document.getElementById('room');
-
-var room = "";
-
-var Jan = 0;
-var Feb = 1;
-var Mar = 2;
-var Apr = 3;
-var May = 4;
-var Jun = 5;
-var Jul = 6;
-var Aug = 7;
-var Sep = 8;
-var Oct = 9;
-var Nov = 10;
-var Dec = 11;
-
 //fetch xlsx file
 //https://1drv.ms/x/s!AptZCRjBU4chgp0Y7p3h28ISD39YJQ
 
 //app id = 3bce3124-41e9-445d-9aff-a3b90b7de430
 
-var myUrl = "https://onedrive.live.com/download?cid=218753C11809599B&resid=218753C11809599B%2136504&authkey=AE6Bl7Dyr-x_Q4s&em=2";
+let myUrl = "https://onedrive.live.com/download?cid=218753C11809599B&resid=218753C11809599B%2136504&authkey=AE6Bl7Dyr-x_Q4s&em=2";
 
-//var myUrl = "https://www.google.com"
+//let myUrl = "https://www.google.com"
 
 // However to make it work, we are going to use the cors-anywhere free service to bypass this
-var proxy = 'https://cors-anywhere.herokuapp.com/';
+let proxy = 'https://cors-anywhere.herokuapp.com/';
 
 // Execute request
-var oReq = new XMLHttpRequest();
+let oReq = new XMLHttpRequest();
 oReq.responseType = "arraybuffer";
 
+function getJsDateFromExcel(excelDate) {
+
+  // JavaScript dates can be constructed by passing milliseconds
+  // since the Unix epoch (January 1, 1970) example: new Date(12312512312);
+
+  // 1. Subtract number of days between Jan 1, 1900 and Jan 1, 1970, plus 1 (Google "excel leap year bug")             
+  // 2. Convert to milliseconds.
+
+	return new Date((excelDate - (25567 + 1))*86400*1000);
+
+}
+
 oReq.addEventListener("load", function () {
-    var arraybuffer = this.response;
+    let arraybuffer = this.response;
 
     // convert data to binary string
-    var data = new Uint8Array(arraybuffer);
-    var arr = new Array();
-    for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-    var bstr = arr.join("");
+    let data = new Uint8Array(arraybuffer);
+    let arr = new Array();
+    for (let i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    let bstr = arr.join("");
 
     // Call XLSX
-    var workbook = XLSX.read(bstr, {
+    let workbook = XLSX.read(bstr, {
         type: "binary"
     });
 
     // DO SOMETHING WITH workbook HERE 
-    var first_sheet_name = workbook.SheetNames[0];
+    let first_sheet_name = workbook.SheetNames[0];
     // Get worksheet 
-    var worksheet = workbook.Sheets[first_sheet_name];
+    let worksheet = workbook.Sheets[first_sheet_name];
     
-    var JSONstuff = XLSX.utils.sheet_to_json(worksheet, {
+    let JSONstuff = XLSX.utils.sheet_to_json(worksheet, {
         raw: true
     });
 
-    console.log(JSONstuff);
+    //console.log(JSONstuff);
 
-    for (let i = 0; i < JSONstuff.length; i++)
+    for (let i = 2; i < JSONstuff.length; i++)
     {
       console.log(JSONstuff[i]);
+      console.log(JSONstuff[i][Object.keys(JSONstuff[i])[1]]);
+      console.log(getJsDateFromExcel(JSONstuff[i][Object.keys(JSONstuff[i])[0]]));
+
+      let mass_date = getJsDateFromExcel(JSONstuff[i][Object.keys(JSONstuff[i])[0]]);
+      let today = new Date();
+
+      if (mass_date > today)
+      {
+        let el = document.getElementById('room');
+
+        let room = "Next Mass: " + mass_date.toDateString() + " in room " + JSONstuff[i][Object.keys(JSONstuff[i])[1]];
+
+        el.innerHTML = room;
+        break;
+      }
     }
 });
 // Or post, etc
 oReq.open("GET", proxy + myUrl);
 oReq.send();
-
-/* set up XMLHttpRequest */
-/*
-var url = "https://cors-anywhere.herokuapp.com/https://jy4pja.dm.files.1drv.com/y4mRryyUBbyj6zi2MkfR2TQ_Xk8kBcBNGfxC7N626TdxSx5r3i_wrOHDqEYDShTroys7--SM0koCKvhdU77FzDXolA_uotPD20akvB34PI963fUwxnPOj9erJSUALSNkcNLRup_ZMbguoFsxkxsztUwIKijGtkJ1t4RbMZr2IX22kcaehVNvyZNPpdWkuV52hZD/spring%20mass%20events.xlsx?download&psid=1";
-var oReq = new XMLHttpRequest();
-oReq.open("GET", url, true);
-oReq.responseType = "arraybuffer";
-
-oReq.onload = function(e) {
-    var arraybuffer = oReq.response;
-
-    // convert data to binary string
-    var data = new Uint8Array(arraybuffer);
-    var arr = new Array();
-    for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-    var bstr = arr.join("");
-
-    // Call XLSX
-    var workbook = XLSX.read(bstr, {
-        type: "binary"
-    });
-
-    // DO SOMETHING WITH workbook HERE 
-    var first_sheet_name = workbook.SheetNames[0];
-    // Get worksheet 
-    var worksheet = workbook.Sheets[first_sheet_name];
-    
-    var JSONstuff = XLSX.utils.sheet_to_json(worksheet, {
-        raw: true
-    });
-
-    console.log(JSONstuff);
-
-    for (let i = 0; i < JSONstuff.length; i++)
-    {
-      console.log(JSONstuff[i]);
-    }
-}
-
-oReq.send();
-
-*/
-if (today.getMonth() == Feb)
-{
-  if (today.getDate() <= 19) room = "Feb 19 in Room LB. 440A";
-  else if (today.getDate() <= 26) room = "next mass date";
-}
-else if (today.getMonth() == Mar)
-{
-  if (today.getDate() <= 5) room = "Mar 5 in ...";
-  else if (today.getDate() <= 12) room = "next mass date";
-}
-
-el.innerHTML = room;
-
 
 
