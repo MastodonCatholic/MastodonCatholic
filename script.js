0866720f3e37ea1ceeb8cb2d17d4437909435f04
MastodonCatholic <langmj02@pfw.edu>
2019-04-14 22:04:35 -0400

    Last change before change to bootstrap. Fixed bg image slideshow

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 //JS and CSS stuff
 
+//Add the list of images
+
+var images = ["Antoni_Estruch-_Sant_Sopar-_Museu_dArt_de_Sabadell.jpg",
+              "00.159.220_PS2.jpg",
+              "00.159.223_PS1.jpg",
+              "1ea2068d15476c63af1a4ef602d03f88 (1).jpg",
+              "IMG_20190219_123937978_BURST000_COVER.jpg",
+              "14ChrismMassFW20.jpg"];
+
+
+var newelement='';
+var delay = 5;
+
+//add image divs
+for (var i = 0; i < images.length; i++)
+{
+  newelement = newelement + '<div class="bg" id="bgimage' + i + '" style="background-image: url(' + images[i] + ');"></div>';
+  console.log(newelement)
+}
+
+document.getElementById("body").innerHTML = newelement + document.getElementById("body").innerHTML;
+
+//loop through on timer and animate
+var bg = images.length-1;
+
+function animateBG()
+{
+
+  if (bg == 0)
+  {
+    for (var i = 1/*NOT zero cuz that's the one on the bottom*/; i < images.length; i++)
+    {
+      document.getElementById("bgimage" + i).style["animation-name"] = "bgfadein";
+      document.getElementById("bgimage" + i).style["animation-duration"] = "2s";
+    }
+
+    bg = images.length-1;
+  }
+  else
+  {
+    document.getElementById("bgimage" + bg).style["animation-name"] = "bgfadeout";
+    document.getElementById("bgimage" + bg).style["animation-duration"] = "2s";
+    
+    bg--;
+  }
+
+  
+}
+
+setInterval(animateBG, delay*1000);
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 
